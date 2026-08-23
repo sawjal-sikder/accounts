b7988baf05dbafb99ee40b69517ffe2a1fbed223
MD. SAWJAL SIKDER <mdsajalsikder@gmail.com>
2026-08-23 15:02:13 +0600

    Add JWT authentication and permissions to REST framework settings; update requirements for djangorestframework-simplejwt

--- config/settings.py
+++ config/settings.py
@@ -134,6 +134,12 @@ MAILERS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
 }
 
 SPECTACULAR_SETTINGS = {
