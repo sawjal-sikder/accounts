09bdc3511406c44d583d065ac64dbd960e5bfe26
MD. SAWJAL SIKDER <mdsajalsikder@gmail.com>
2026-08-23 15:41:52 +0600

    Add extend_schema decorator to Group views for API documentation

--- accounts/views/group/group.py
+++ accounts/views/group/group.py
@@ -2,8 +2,20 @@ from rest_framework import permissions
 from rest_framework import generics
 from accounts.models import AccountGroup
 from accounts.serializers.group.group import GroupSerializer
+from drf_spectacular.utils import extend_schema
+from config.pagination import CustomPagination
 
+
+@extend_schema(tags=['Account Groups'])
 class GroupListCreateView(generics.ListCreateAPIView):
+    queryset = AccountGroup.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = CustomPagination
+    
+    
+@extend_schema(tags=['Account Groups'])
+class GroupRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = AccountGroup.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
