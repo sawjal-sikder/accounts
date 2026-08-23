fbaf508630b6d9b94ee2c53ba9b7a243a5f40e1c
MD. SAWJAL SIKDER <mdsajalsikder@gmail.com>
2026-08-23 20:48:02 +0600

    Refactor Group views to filter active AccountGroups and track user actions on create and update

--- accounts/views/group/group.py
+++ accounts/views/group/group.py
@@ -8,14 +8,27 @@ from config.pagination import CustomPagination
 
 @extend_schema(tags=['Account Groups'])
 class GroupListCreateView(generics.ListCreateAPIView):
-    queryset = AccountGroup.objects.all()
+    queryset = AccountGroup.objects.filter(is_active=True)
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = CustomPagination
     
+    def perform_create(self, serializer):
+        serializer.save(
+            created_by=self.request.user, 
+            updated_by=self.request.user
+            )
+    
     
 @extend_schema(tags=['Account Groups'])
 class GroupRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = AccountGroup.objects.all()
+    queryset = AccountGroup.objects.filter(is_active=True)
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
+        
+    def perform_update(self, serializer):
+        serializer.save(updated_by=self.request.user)
