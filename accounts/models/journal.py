49c33a5ff77bc59cc8771f468b4b6992e604356c
MD. SAWJAL SIKDER <mdsajalsikder@gmail.com>
2026-09-03 19:30:32 +0600

    Refactor Journal model: format reference field and ensure reference generation on save

--- accounts/models/journal.py
+++ accounts/models/journal.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 
-
 class Journal(models.Model):
     date = models.DateField()
-    reference = models.CharField(max_length=100,blank=True)
+
+    reference = models.CharField(
+        max_length=100,
+        blank=True,
+    )
+
     description = models.TextField(blank=True)
     is_posted = models.BooleanField(default=False)
 
@@ -16,3 +20,14 @@ class Journal(models.Model):
 
     def __str__(self):
         return f"Journal #{self.id} - {self.date}"
+
+    def save(self, *args, **kwargs):
+        is_new = self.pk is None
+
+        # First save to generate ID
+        super().save(*args, **kwargs)
+
+        # Generate reference after ID exists
+        if is_new and not self.reference:
+            self.reference = f"JRN-{self.date.strftime('%Y-%m-%d')}-{self.id}"
+            super().save(update_fields=["reference"])
