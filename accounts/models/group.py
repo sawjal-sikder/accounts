2091f2ef6a03627864ce3e1cafce5138cc3dc55b
sawjal <mdsajalsikder@gmail.com>
2026-09-07 08:57:09 +0600

    feat: Update AccountGroup model to include 'Cost of Goods Sold' in group_type choices

--- accounts/models/group.py
+++ accounts/models/group.py
@@ -9,6 +9,7 @@ class AccountGroup(models.Model):
         LIABILITY = "liability", "Liability"
         EQUITY = "equity", "Equity"
         REVENUE = "revenue", "Revenue"
+        COST_OF_GOODS_SOLD = "cogs", "Cost of Goods Sold"
         EXPENSE = "expense", "Expense"
 
     name = models.CharField(max_length=100)
