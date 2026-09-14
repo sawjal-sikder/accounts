53d802cd617cffc07d3be076ac050c5e2409cd36
MD. SAWJAL SIKDER <mdsajalsikder@gmail.com>
2026-09-14 20:46:45 +0600

    feat: Enhance JournalSerializer create method to generate reference based on type_of_journal

--- accounts/serializers/journal/serializers.py
+++ accounts/serializers/journal/serializers.py
@@ -3,43 +3,84 @@ from accounts.models.journal import Journal
 from accounts.serializers.journalline.serializers import JournalLineSerializer
 
 class JournalSerializer(serializers.ModelSerializer):
-    
+
+    JOURNAL_PREFIXES = {
+        "general": "JRN",
+        "sales": "SAL",
+        "sales_return": "SRT",
+        "purchase": "PUR",
+        "purchase_return": "PRT",
+        "received_payment": "REC",
+        "made_payment": "PAY",
+        "contra": "CON",
+        "expense": "EXP",
+        "income": "INC",
+        "payroll": "PAY",
+        "tax": "TAX",
+        "depreciation": "DEP",
+        "accrual": "ACC",
+        "prepayment": "PRE",
+        "inventory": "INV",
+        "adjustment": "ADJ",
+        "opening_balance": "OPB",
+        "closing": "CLS",
+        "credit_note": "CRN",
+        "debit_note": "DBN",
+        "refund": "REF",
+        "write_off": "WRO",
+        "transfer": "TRF",
+    }
+
     class Meta:
         model = Journal
         fields = [
-            'id',
-            'date',
-            'reference',
-            'description',
-            'is_posted',
-            'is_active',
-            'created_by',
-            'updated_by',
-            'created_at',
-            'updated_at',
+            "id",
+            "date",
+            "reference",
+            "type_of_journal",
+            "description",
+            "is_posted",
+            "is_active",
+            "created_by",
+            "updated_by",
+            "created_at",
+            "updated_at",
         ]
-        read_only_fields =[
-            'id',
-            'is_active',
-            'created_by',
-            'updated_by',
-            'created_at',
-            'updated_at',
+
+        read_only_fields = [
+            "id",
+            "is_active",
+            "created_by",
+            "updated_by",
+            "created_at",
+            "updated_at",
         ]
-        
-        def create(self, validated_data):
-            queryset = Journal.objects.create(**validated_data)
-            reference = validated_data.get('reference')
-            if reference:
-                queryset.reference = reference
-                queryset.save()
-            
-            else:
-                date_str = queryset.date.strftime('%Y%m%d')
-                queryset.reference = f"JRN-{date_str}-{queryset.id}"
-                queryset.save()
-                
-            return queryset
+
+    def create(self, validated_data):
+        journal = Journal.objects.create(**validated_data)
+
+        # If reference is not provided
+        if not journal.reference:
+            prefix = self.JOURNAL_PREFIXES.get(
+                journal.type_of_journal,
+                "JRN"
+            )
+
+            date_str = journal.date.strftime("%Y%m%d")
+
+            journal.reference = f"{prefix}-{date_str}-{journal.id}"
+
+            journal.save(update_fields=["reference"])
+
+        return journal
+    
+    
+    
+    
+    
+    
+    
+    
            
         
         
