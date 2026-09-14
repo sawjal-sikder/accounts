66aa35ec37ed1dfc981a9e76c4380e99c7be5b69
MD. SAWJAL SIKDER <mdsajalsikder@gmail.com>
2026-09-14 20:31:20 +0600

    feat: Refactor JournalSerializer to improve create method and read_only_fields

--- accounts/serializers/journal/serializers.py
+++ accounts/serializers/journal/serializers.py
@@ -3,6 +3,7 @@ from accounts.models.journal import Journal
 from accounts.serializers.journalline.serializers import JournalLineSerializer
 
 class JournalSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Journal
         fields = [
@@ -17,6 +18,29 @@ class JournalSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
         ]
+        read_only_fields =[
+            'id',
+            'is_active',
+            'created_by',
+            'updated_by',
+            'created_at',
+            'updated_at',
+        ]
+        
+        def create(self, validated_data):
+            queryset = Journal.objects.create(**validated_data)
+            reference = validated_data.get('reference')
+            if reference:
+                queryset.reference = reference
+                queryset.save()
+            
+            else:
+                date_str = queryset.date.strftime('%Y%m%d')
+                queryset.reference = f"JRN-{date_str}-{queryset.id}"
+                queryset.save()
+                
+            return queryset
+           
         
         
 class JournalDetailSerializer(serializers.ModelSerializer):
